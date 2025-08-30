e2159099646cdbd5e8b8b2532a5d0d994b9a5777
장완중 <tjdrj530@naver.com>
2025-08-30 19:20:10 +0900

    feature: springboot 프로젝트 생성

---
+++ src/main/java/katecam/hyuswim/HyuswimApplication.java
@@ -0,0 +1,13 @@
+package katecam.hyuswim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyuswimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyuswimApplication.class, args);
+    }
+
+}
