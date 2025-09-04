236da2d5d4915dc9595c098c57a7507d8f4a32d0
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-04 21:22:03 +0900

    refactor(domain): Board, User 엔티티에 Lombok @Getter 추가

--- src/main/java/katecam/hyuswim/board/domain/Board.java
+++ src/main/java/katecam/hyuswim/board/domain/Board.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import katecam.hyuswim.comment.Comment;
 import katecam.hyuswim.like.Like;
 import katecam.hyuswim.user.User;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class Board {
