0e7d0d214fb4d3aec18e4a389aafe3d6e5cc495e
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-05 22:56:07 +0900

    feat(post) : 게시물 검색 기능에 기간별 검색 기능 추가

--- src/main/java/katecam/hyuswim/post/repository/PostRepository.java
+++ src/main/java/katecam/hyuswim/post/repository/PostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,10 +16,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByIsDeletedFalse(Pageable pageable);
     Optional<Post> findByIdAndIsDeletedFalse(Long id);
     Page<Post> findByCategoryAndIsDeletedFalse(PostCategory category, Pageable pageable);
-    @Query("SELECT p FROM Post p WHERE p.isDeleted = false AND (:category IS NULL OR p.category = :category) AND (:keyword IS NULL OR p.title LIKE %:keyword% OR p.content LIKE %:keyword%)")
-    Page<Post> searchByCategoryAndKeyword(
+    @Query("SELECT p FROM Post p WHERE p.isDeleted = false AND (:category IS NULL OR p.category = :category) AND (:keyword IS NULL OR p.title LIKE %:keyword% OR p.content LIKE %:keyword%) AND (:startDate IS NULL OR p.createdAt >= :startDate) AND (:endDate IS NULL OR p.createdAt <= :endDate)")
+    Page<Post> searchByCategoryAndKeywordAndPeriod(
             @Param("category") PostCategory category,
             @Param("keyword") String keyword,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
             Pageable pageable
     );
 }
