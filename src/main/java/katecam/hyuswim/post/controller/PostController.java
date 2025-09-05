c1461573b8cb934f22094217c89f3cdfc56e3263
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-05 23:39:55 +0900

    chore: 불필요한 import 제거

--- src/main/java/katecam/hyuswim/post/controller/PostController.java
+++ src/main/java/katecam/hyuswim/post/controller/PostController.java
@@ -8,12 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
