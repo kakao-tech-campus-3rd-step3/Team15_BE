2d704c7366425359b3a00b673e10349e1278653f
장완중 <tjdrj530@naver.com>
2025-09-05 17:40:16 +0900

    feature: 로그인 구현
    - jwt 의존성 설정
    - 유저가없을때 예외처리 구현
    - jwt기본 설정
    - 로그인 성공시 jwt토큰 발급

---
+++ src/main/java/katecam/hyuswim/user/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package katecam.hyuswim.user.exception;
+
+import javax.naming.AuthenticationException;
+
+public class UserNotFoundException extends AuthenticationException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/katecam/hyuswim/user/jwt/JwtUtil.java
@@ -0,0 +1,65 @@
+package katecam.hyuswim.user.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import katecam.hyuswim.user.dto.JwtTokenRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private JwtParser jwtParser;
+
+    private final long oneHour = 60 * 60 * 1000;
+
+    @PostConstruct
+    public void init() {
+        jwtParser = Jwts.parser().setSigningKey(secret).build();
+    }
+
+    public String extractEmail(String token){
+        return jwtParser
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+    public String generateToken(JwtTokenRequest jwtTokenRequest) {
+        long currentTimeMillis = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(jwtTokenRequest.getUsername())
+                .claim("role", jwtTokenRequest.getRole())
+                .setIssuedAt(new Date(currentTimeMillis))
+                .setExpiration(new Date(currentTimeMillis + oneHour))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            jwtParser.parseClaimsJws(token); // 서명 검증
+            return !isTokenExpired(token);
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+
+    private Boolean isTokenExpired(String token) {
+        return jwtParser
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
