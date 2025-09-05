a5178d2d6a542393c341b1fd9783fbe6c7764b43
장완중 <tjdrj530@naver.com>
2025-09-05 17:40:16 +0900

    feature: 로그인 구현
    - jwt 의존성 설정
    - 유저가없을때 예외처리 구현
    - jwt기본 설정
    - 로그인 성공시 jwt토큰 발급

--- src/main/java/katecam/hyuswim/user/service/UserService.java
+++ src/main/java/katecam/hyuswim/user/service/UserService.java
@@ -1,11 +1,16 @@
 package katecam.hyuswim.user.service;
 
+import katecam.hyuswim.user.User;
+import katecam.hyuswim.user.dto.LoginRequest;
 import katecam.hyuswim.user.dto.SignupRequest;
+import katecam.hyuswim.user.exception.UserNotFoundException;
 import katecam.hyuswim.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,4 +30,26 @@ public class UserService {
         userRepository.save(signupRequest.toEntity());
 
     }
+
+    @Transactional
+    public boolean existUser(LoginRequest loginRequest) {
+        Optional<User> userOptional = userRepository.findByUsername(loginRequest.getUsername());
+
+        if (userOptional.isEmpty()) {
+            return false;
+        }
+
+        User user = userOptional.get();
+        return bCryptPasswordEncoder.matches(loginRequest.getPassword(), user.getPassword());
+    }
+
+    @Transactional
+    public User findUserByUsername(String username) throws UserNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if(userOptional.isEmpty()) {
+            throw new UserNotFoundException("해당 유저는 존재하지 않습니다.");
+        }
+        return userOptional.get();
+    }
+
 }
