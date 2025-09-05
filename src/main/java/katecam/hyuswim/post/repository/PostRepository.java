049a68c1363ed0fd325da1b76272527bebb3e976
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-05 23:16:05 +0900

    fix: PostRepository 검색 쿼리 필드명 수정

--- src/main/java/katecam/hyuswim/post/repository/PostRepository.java
+++ src/main/java/katecam/hyuswim/post/repository/PostRepository.java
@@ -21,7 +21,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p " +
             "WHERE p.isDeleted = false " +
-            "AND (:category IS NULL OR p.category = :category) " +
+            "AND (:category IS NULL OR p.postCategory = :category) " +
             "AND (:keyword IS NULL OR p.title LIKE CONCAT('%', :keyword, '%') OR p.content LIKE CONCAT('%', :keyword, '%')) " +
             "AND (:startDate IS NULL OR p.createdAt >= :startDate) " +
             "AND (:endDate IS NULL OR p.createdAt <= :endDate)")
