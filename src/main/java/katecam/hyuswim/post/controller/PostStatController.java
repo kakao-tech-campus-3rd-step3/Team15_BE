5f3540c8eb4da40ce75db33a7a21c3275150405e
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-07 01:01:45 +0900

    refactor(controller): PostController와 PostStatController에서 ApiResponse 제거 및 ResponseEntity 기반으로 변경

--- src/main/java/katecam/hyuswim/post/controller/PostStatController.java
+++ src/main/java/katecam/hyuswim/post/controller/PostStatController.java
@@ -1,9 +1,9 @@
 package katecam.hyuswim.post.controller;
 
-import katecam.hyuswim.common.ApiResponse;
 import katecam.hyuswim.post.dto.PostStatsResponse;
 import katecam.hyuswim.post.service.PostStatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class PostStatController {
     private final PostStatService postStatService;
 
     @GetMapping("/stats")
-    public ApiResponse<PostStatsResponse> getPostStats() {
-        return ApiResponse.success(postStatService.getPostStats());
+    public ResponseEntity<PostStatsResponse> getPostStats() {
+        return ResponseEntity.ok(postStatService.getPostStats());
     }
 }
