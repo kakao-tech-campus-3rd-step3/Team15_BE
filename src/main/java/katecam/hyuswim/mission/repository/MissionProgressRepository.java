ed030419e87ef3a5ad11c904db1346a7d3526962
kimpro4214 <qnfghk555@naver.com>
2025-09-08 14:03:32 +0900

    style: 미션 코드 스타일 적용

--- src/main/java/katecam/hyuswim/mission/repository/MissionProgressRepository.java
+++ src/main/java/katecam/hyuswim/mission/repository/MissionProgressRepository.java
@@ -1,11 +1,7 @@
 package katecam.hyuswim.mission.repository;
 
-import katecam.hyuswim.mission.progress.MissionProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.util.Optional;
+import katecam.hyuswim.mission.progress.MissionProgress;
 
-public interface MissionProgressRepository extends JpaRepository<MissionProgress, Long> {
-
-}
+public interface MissionProgressRepository extends JpaRepository<MissionProgress, Long> {}
