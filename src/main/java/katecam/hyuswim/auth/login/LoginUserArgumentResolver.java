0d204ab9adaffac9f5e8fb0cb3d263da54254a33
장완중 <tjdrj530@naver.com>
2025-09-09 15:55:13 +0900

    style: 코드스타일 적용

--- src/main/java/katecam/hyuswim/auth/login/LoginUserArgumentResolver.java
+++ src/main/java/katecam/hyuswim/auth/login/LoginUserArgumentResolver.java
@@ -1,14 +1,5 @@
 package katecam.hyuswim.auth.login;
 
-import jakarta.servlet.http.HttpServletRequest;
-import katecam.hyuswim.auth.jwt.JwtUtil;
-import katecam.hyuswim.common.error.CustomException;
-import katecam.hyuswim.common.error.ErrorCode;
-import katecam.hyuswim.user.User;
-import katecam.hyuswim.user.exception.UserNotFoundException;
-import katecam.hyuswim.user.repository.UserRepository;
-import katecam.hyuswim.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -16,40 +7,49 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import jakarta.servlet.http.HttpServletRequest;
+import katecam.hyuswim.auth.jwt.JwtUtil;
+import katecam.hyuswim.common.error.CustomException;
+import katecam.hyuswim.common.error.ErrorCode;
+import katecam.hyuswim.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private final JwtUtil jwtUtil;
-    private final UserRepository userRepository;
+  private final JwtUtil jwtUtil;
+  private final UserRepository userRepository;
 
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.hasParameterAnnotation(LoginUser.class);
+  }
 
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(LoginUser.class);
+  @Override
+  public Object resolveArgument(
+      MethodParameter parameter,
+      ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest,
+      WebDataBinderFactory binderFactory)
+      throws Exception {
+
+    HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+    String bearerToken = request.getHeader("Authorization");
+
+    if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+      throw new CustomException(ErrorCode.INVALID_TOKEN);
     }
 
-    @Override
-    public Object resolveArgument(MethodParameter parameter,
-                                  ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory) throws Exception {
+    String token = bearerToken.substring(7);
 
-        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        String bearerToken = request.getHeader("Authorization");
-
-        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
-            throw new CustomException(ErrorCode.INVALID_TOKEN);
-        }
-
-        String token = bearerToken.substring(7);
-
-        if (!jwtUtil.validateToken(token)) {
-            throw new CustomException(ErrorCode.INVALID_TOKEN);
-        }
-        String email = jwtUtil.extractEmail(token);
-
-        return userRepository.findByEmail(email).orElseThrow(() ->
-                new CustomException(ErrorCode.USER_NOT_FOUND));
+    if (!jwtUtil.validateToken(token)) {
+      throw new CustomException(ErrorCode.INVALID_TOKEN);
     }
+    String email = jwtUtil.extractEmail(token);
+
+    return userRepository
+        .findByEmail(email)
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+  }
 }
