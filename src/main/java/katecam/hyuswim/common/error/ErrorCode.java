7999928d132d441fb5139ba62854d94402f43a62
장완중 <tjdrj530@naver.com>
2025-09-09 15:10:28 +0900

    feat: 로그인유저 어노테이션 구현
    - jwt토큰 인증 구현
    - MISSING_JWT_TOKEN 예외 추가

--- src/main/java/katecam/hyuswim/common/error/ErrorCode.java
+++ src/main/java/katecam/hyuswim/common/error/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
   POST_ACCESS_DENIED(HttpStatus.FORBIDDEN, "작성자만 게시글을 수정/삭제할 수 있습니다."),
   LOGIN_FALSE(HttpStatus.FORBIDDEN, "아이디 또는 비밀번호가 잘못되었습니다"),
   COMMENT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "작성자만 댓글을 수정/삭제할 수 있습니다."),
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다.");
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다."),
+  MISSING_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "JWT토큰이 없거나 유효하지 않습니다");
 
   private final HttpStatus status;
   private final String message;
