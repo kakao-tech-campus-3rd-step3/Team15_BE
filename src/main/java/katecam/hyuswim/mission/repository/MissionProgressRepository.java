85484dce5fa44dde905b5c1044742720e8033ac7
hwangyungyu <vlzkcbdmdkr@naver.com>
2025-09-11 00:08:54 +0900

    refactor : MissionProgressRepository로 책임 분리

--- src/main/java/katecam/hyuswim/mission/repository/MissionProgressRepository.java
+++ src/main/java/katecam/hyuswim/mission/repository/MissionProgressRepository.java
@@ -4,4 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import katecam.hyuswim.mission.progress.MissionProgress;
 
-public interface MissionProgressRepository extends JpaRepository<MissionProgress, Long> {}
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface MissionProgressRepository extends JpaRepository<MissionProgress, Long> {
+    long countByUser_IdAndProgressDate(Long userId, LocalDate date);
+
+    Optional<MissionProgress> findFirstByUser_IdAndProgressDate(Long userId, LocalDate date);
+
+    Optional<MissionProgress> findFirstByUser_IdAndMission_IdAndProgressDate(Long userId, Long missionId, LocalDate date);
+
+    long countByMission_IdAndProgressDate(Long missionId, LocalDate date);
+
+    long countByMission_IdAndProgressDateAndIsCompletedTrue(Long missionId, LocalDate date);
+}
