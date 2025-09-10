7030dc2477fb34605fabd41a387a5fd48f8d3eea
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-11 00:00:28 +0900

    refactor : comment에 @LoginUser 적용

--- src/main/java/katecam/hyuswim/comment/controller/CommentController.java
+++ src/main/java/katecam/hyuswim/comment/controller/CommentController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import katecam.hyuswim.auth.login.LoginUser;
 import katecam.hyuswim.comment.dto.CommentDetailResponse;
 import katecam.hyuswim.comment.dto.CommentListResponse;
 import katecam.hyuswim.comment.dto.CommentRequest;
 import katecam.hyuswim.comment.service.CommentService;
 import katecam.hyuswim.post.dto.PageResponse;
+import katecam.hyuswim.user.User;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,38 +21,43 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentController {
 
-  private final CommentService commentService;
+    private final CommentService commentService;
 
-  @PostMapping("/posts/{postId}/comments")
-  public ResponseEntity<CommentDetailResponse> createComment(
-      @PathVariable Long postId, @RequestParam Long userId, @RequestBody CommentRequest request) {
-    CommentDetailResponse response = commentService.createComment(request, userId, postId);
-    return ResponseEntity.status(HttpStatus.CREATED).body(response);
-  }
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDetailResponse> createComment(
+            @PathVariable Long postId,
+            @LoginUser User user,
+            @RequestBody CommentRequest request) {
+        CommentDetailResponse response = commentService.createComment(request, user, postId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
-  @GetMapping("/posts/{postId}/comments")
-  public ResponseEntity<PageResponse<CommentListResponse>> getComments(
-      @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC, size = 10)
-          Pageable pageable) {
-    return ResponseEntity.ok(commentService.getComments(pageable));
-  }
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<PageResponse<CommentListResponse>> getComments(
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC, size = 10)
+            Pageable pageable) {
+        return ResponseEntity.ok(commentService.getComments(pageable));
+    }
 
-  @GetMapping("/comments/{id}")
-  public ResponseEntity<CommentDetailResponse> getComment(@PathVariable Long id) {
-    CommentDetailResponse response = commentService.getComment(id);
-    return ResponseEntity.ok(response);
-  }
+    @GetMapping("/comments/{id}")
+    public ResponseEntity<CommentDetailResponse> getComment(@PathVariable Long id) {
+        CommentDetailResponse response = commentService.getComment(id);
+        return ResponseEntity.ok(response);
+    }
 
-  @PatchMapping("/comments/{id}")
-  public ResponseEntity<CommentDetailResponse> updateComment(
-      @PathVariable Long id, @RequestParam Long userId, @RequestBody CommentRequest request) {
-    CommentDetailResponse response = commentService.updateComment(id, userId, request);
-    return ResponseEntity.ok(response);
-  }
+    @PatchMapping("/comments/{id}")
+    public ResponseEntity<CommentDetailResponse> updateComment(
+            @PathVariable Long id,
+            @LoginUser User user,
+            @RequestBody CommentRequest request) {
+        CommentDetailResponse response = commentService.updateComment(id, user, request);
+        return ResponseEntity.ok(response);
+    }
 
-  @DeleteMapping("/comments/{id}")
-  public ResponseEntity<Void> deletePost(@PathVariable Long id, @RequestParam Long userId) {
-    commentService.deleteComment(id, userId);
-    return ResponseEntity.noContent().build();
-  }
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id, @LoginUser User user) {
+        commentService.deleteComment(id, user);
+        return ResponseEntity.noContent().build();
+    }
 }
+
