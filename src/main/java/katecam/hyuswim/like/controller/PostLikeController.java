a72baf337cfc39a5807bff30bf825bd8be477fd9
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-11 00:04:56 +0900

    refator : Like에 @LoginUser 적용

--- src/main/java/katecam/hyuswim/like/controller/PostLikeController.java
+++ src/main/java/katecam/hyuswim/like/controller/PostLikeController.java
@@ -3,7 +3,9 @@ package katecam.hyuswim.like.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import katecam.hyuswim.auth.login.LoginUser;
 import katecam.hyuswim.like.service.PostLikeService;
+import katecam.hyuswim.user.User;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -11,17 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostLikeController {
 
-  private final PostLikeService postLikeService;
+    private final PostLikeService postLikeService;
 
-  @PostMapping("/posts/{postId}/likes")
-  public ResponseEntity<Void> addLike(@PathVariable Long postId, @RequestParam Long userId) {
-    postLikeService.addLike(postId, userId);
-    return ResponseEntity.ok().build();
-  }
+    @PostMapping("/posts/{postId}/likes")
+    public ResponseEntity<Void> addLike(@PathVariable Long postId, @LoginUser User user) {
+        postLikeService.addLike(postId, user);
+        return ResponseEntity.ok().build();
+    }
 
-  @DeleteMapping("/posts/{postId}/likes")
-  public ResponseEntity<Void> deleteLike(@PathVariable Long postId, @RequestParam Long userId) {
-    postLikeService.deleteLike(postId, userId);
-    return ResponseEntity.noContent().build();
-  }
+    @DeleteMapping("/posts/{postId}/likes")
+    public ResponseEntity<Void> deleteLike(@PathVariable Long postId, @LoginUser User user) {
+        postLikeService.deleteLike(postId, user);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/katecam/hyuswim/like/service/PostLikeService.java
+++ src/main/java/katecam/hyuswim/like/service/PostLikeService.java
@@ -1,7 +1,6 @@
 package katecam.hyuswim.like.service;
 
 import org.springframework.stereotype.Service;
-
 import jakarta.transaction.Transactional;
 import katecam.hyuswim.common.error.CustomException;
 import katecam.hyuswim.common.error.ErrorCode;
@@ -10,39 +9,33 @@ import katecam.hyuswim.like.repository.PostLikeRepository;
 import katecam.hyuswim.post.domain.Post;
 import katecam.hyuswim.post.repository.PostRepository;
 import katecam.hyuswim.user.User;
-import katecam.hyuswim.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PostLikeService {
 
-  private final PostLikeRepository postLikeRepository;
-  private final PostRepository postRepository;
-  private final UserRepository userRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
 
-  @Transactional
-  public void addLike(Long postId, Long userId) {
+    @Transactional
+    public void addLike(Long postId, User user) {
+        Post post =
+                postRepository
+                        .findById(postId)
+                        .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
-    Post post =
-        postRepository
-            .findById(postId)
-            .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-    User user =
-        userRepository
-            .findById(userId)
-            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        postLikeRepository.save(new PostLike(post, user));
+    }
 
-    postLikeRepository.save(new PostLike(post, user));
-  }
+    @Transactional
+    public void deleteLike(Long postId, User user) {
+        PostLike postLike =
+                postLikeRepository
+                        .findByPostIdAndUserId(postId, user.getId())
+                        .orElseThrow(() -> new CustomException(ErrorCode.LIKE_NOT_FOUND));
 
-  @Transactional
-  public void deleteLike(Long postId, Long userId) {
-    PostLike postLike =
-        postLikeRepository
-            .findByPostIdAndUserId(postId, userId)
-            .orElseThrow(() -> new CustomException(ErrorCode.LIKE_NOT_FOUND));
-
-    postLikeRepository.delete(postLike);
-  }
+        postLikeRepository.delete(postLike);
+    }
 }
+
