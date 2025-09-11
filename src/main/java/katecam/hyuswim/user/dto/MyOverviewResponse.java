3fbfe2b8f8cabe35bd6e7b22dba1abc583bf31f4
장완중 <tjdrj530@naver.com>
2025-09-11 15:06:24 +0900

    feat: 마이페이지 오버뷰 구현

---
+++ src/main/java/katecam/hyuswim/user/dto/MyOverviewResponse.java
@@ -0,0 +1,15 @@
+package katecam.hyuswim.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MyOverviewResponse {
+
+    private int postCount;
+    private int commentCount;
+    private int likesCount;
+    private int MissionsCount;
+    private String email;
+}
