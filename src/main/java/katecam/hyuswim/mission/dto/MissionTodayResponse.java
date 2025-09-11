1450437c92cf75860bca388b286253a331c15b23
yungyu <168967249+hwangyungyu@users.noreply.github.com>
2025-09-11 15:05:22 +0900

    style : 코드 스타일

--- src/main/java/katecam/hyuswim/mission/dto/MissionTodayResponse.java
+++ src/main/java/katecam/hyuswim/mission/dto/MissionTodayResponse.java
@@ -23,27 +23,40 @@ public class MissionTodayResponse {
   // 사용자 상태
   private TodayState state;
 
-    private MissionTodayResponse(
-            Long id, String title, String content, Long point,
-            MissionCategory category, MissionLevel level, boolean active,
-            long todayStartedCount, long todayCompletedCount, TodayState state) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.point = point;
-        this.category = category;
-        this.level = level;
-        this.active = active;
-        this.todayStartedCount = todayStartedCount;
-        this.todayCompletedCount = todayCompletedCount;
-        this.state = state;
-    }
+  private MissionTodayResponse(
+      Long id,
+      String title,
+      String content,
+      Long point,
+      MissionCategory category,
+      MissionLevel level,
+      boolean active,
+      long todayStartedCount,
+      long todayCompletedCount,
+      TodayState state) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.point = point;
+    this.category = category;
+    this.level = level;
+    this.active = active;
+    this.todayStartedCount = todayStartedCount;
+    this.todayCompletedCount = todayCompletedCount;
+    this.state = state;
+  }
 
-    public static MissionTodayResponse of(Mission m, long started, long completed, TodayState state) {
-        return new MissionTodayResponse(
-                m.getId(), m.getTitle(), m.getContent(), m.getPoint(),
-                m.getCategory(), m.getLevel(), m.isActive(),
-                started, completed, state
-        );
+  public static MissionTodayResponse of(Mission m, long started, long completed, TodayState state) {
+    return new MissionTodayResponse(
+        m.getId(),
+        m.getTitle(),
+        m.getContent(),
+        m.getPoint(),
+        m.getCategory(),
+        m.getLevel(),
+        m.isActive(),
+        started,
+        completed,
+        state);
   }
 }
