add573d73747ad9b4ab225cff6b893d4d4aa33b9
장완중 <tjdrj530@naver.com>
2025-09-11 15:32:21 +0900

    feat: 마이페이지 게시글 조회 구현

--- src/main/java/katecam/hyuswim/user/service/MyPageService.java
+++ src/main/java/katecam/hyuswim/user/service/MyPageService.java
@@ -1,12 +1,17 @@
 package katecam.hyuswim.user.service;
 
 import katecam.hyuswim.like.repository.PostLikeRepository;
+import katecam.hyuswim.post.domain.Post;
 import katecam.hyuswim.user.User;
 import katecam.hyuswim.user.dto.MyOverviewResponse;
+import katecam.hyuswim.user.dto.MyPostListReponse;
 import katecam.hyuswim.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +28,16 @@ public class MyPageService {
         int missionCount = loginUser.getMissionProgresses().size();
 
         return new MyOverviewResponse(postCount,commentCount,likeCount,missionCount,email);
+    }
+
+    @Transactional
+    public List<MyPostListReponse> selectMyPostList(User loginUser) {
+        List<Post> posts = loginUser.getPosts();
+        List<MyPostListReponse> myPostListReponseList = new ArrayList<>();
+        for (Post post : posts) {
+            myPostListReponseList.add(MyPostListReponse.from(post));
+        }
+        return myPostListReponseList;
     }
 
     @Transactional
