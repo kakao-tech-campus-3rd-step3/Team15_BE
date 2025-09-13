68fbb0e0ed3a0b3443a66a51c0d7dcbdbb127e05
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-14 03:05:59 +0900

    feat : 대댓글이 달린 댓글 삭제 시 삭제된 댓글입니다 반환하도록 추가

--- src/main/java/katecam/hyuswim/comment/dto/CommentListResponse.java
+++ src/main/java/katecam/hyuswim/comment/dto/CommentListResponse.java
@@ -18,16 +18,20 @@ public class CommentListResponse {
   private Boolean isAnonymous;
   private LocalDateTime createdAt;
   private LocalDateTime updatedAt;
+  private boolean hasChildren;
 
-  public static CommentListResponse from(Comment entity) {
-    return CommentListResponse.builder()
-        .id(entity.getId())
-        .postId(entity.getPost().getId())
-        .author(entity.getUser().getNickname())
-        .content(entity.getContent())
-        .isAnonymous(entity.getIsAnonymous())
-        .createdAt(entity.getCreatedAt())
-        .updatedAt(entity.getUpdatedAt())
-        .build();
-  }
+    public static CommentListResponse from(Comment comment, boolean hasChildren) {
+        return CommentListResponse.builder()
+                .id(comment.getId())
+                .postId(comment.getPost().getId())
+                .author(comment.getUser().getNickname())
+                .content(comment.getIsDeleted()
+                        ? (hasChildren ? "삭제된 댓글입니다." : null)
+                        : comment.getContent())
+                .isAnonymous(comment.getIsAnonymous())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .hasChildren(hasChildren)
+                .build();
+    }
 }
