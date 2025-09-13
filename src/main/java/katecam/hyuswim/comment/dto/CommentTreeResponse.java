dbb77d8448c784ba8f4de142a2787713a135e567
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-14 02:42:08 +0900

    feat : CommentTreeResponse에 postId, createdAt, updateAt 추가

--- src/main/java/katecam/hyuswim/comment/dto/CommentTreeResponse.java
+++ src/main/java/katecam/hyuswim/comment/dto/CommentTreeResponse.java
@@ -4,23 +4,30 @@ import katecam.hyuswim.comment.domain.Comment;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class CommentTreeResponse {
     private final Long id;
+    private final Long postId;
     private final String content;
     private final String author;
     private final boolean isDeleted;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
     private final List<CommentTreeResponse> children;
 
     public static CommentTreeResponse from(Comment comment){
         return new CommentTreeResponse(
                 comment.getId(),
+                comment.getPost().getId(),
                 comment.getContent(),
                 comment.getUser().getNickname(),
                 comment.getIsDeleted(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt(),
                 comment.getChildren().stream()
                         .map(katecam.hyuswim.comment.dto.CommentTreeResponse::from)
                         .toList()
