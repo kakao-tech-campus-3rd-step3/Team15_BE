fcd9ba6bd8c3af0df108a61c18bcd094186dc9cc
장완중 <tjdrj530@naver.com>
2025-09-14 17:45:39 +0900

    feat: 회원 탈퇴 api 구현

--- src/main/java/katecam/hyuswim/user/controller/UserController.java
+++ src/main/java/katecam/hyuswim/user/controller/UserController.java
@@ -1,7 +1,10 @@
 package katecam.hyuswim.user.controller;
 
+import katecam.hyuswim.auth.login.LoginUser;
+import katecam.hyuswim.user.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,8 @@ import katecam.hyuswim.user.dto.LoginRequest;
 import katecam.hyuswim.user.dto.SignupRequest;
 import katecam.hyuswim.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,4 +36,10 @@ public class UserController {
     String jwtToken = userService.login(loginRequest);
     return ResponseEntity.ok(new JwtTokenResponse(jwtToken));
   }
+
+    @DeleteMapping("/api/user")
+    public ResponseEntity<Void> deleteUser(@LoginUser User loginUser, @RequestBody Map<String, String> requestMap) {
+      userService.deleteUser(loginUser, requestMap.get("confirmText"));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
