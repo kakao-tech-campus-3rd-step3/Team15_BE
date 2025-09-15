8a5be27519227e60f06bcf0f2d295e2bc38a8ba1
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-15 16:37:43 +0900

    feat : AuthorTagResolver에 AI tag 구분 추가

--- src/main/java/katecam/hyuswim/comment/domain/AuthorTagResolver.java
+++ src/main/java/katecam/hyuswim/comment/domain/AuthorTagResolver.java
@@ -2,12 +2,16 @@ package katecam.hyuswim.comment.domain;
 
 import katecam.hyuswim.post.domain.Post;
 import katecam.hyuswim.user.User;
+import katecam.hyuswim.user.UserRole;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorTagResolver {
-    public AuthorTag resolve(User user, Post post){
-        if (user.getId().equals(post.getUser().getId())){
+    public AuthorTag resolve(User user, Post post) {
+        if (user.getRole() == UserRole.AI) {
+            return AuthorTag.AI;
+        }
+        if (user.getId().equals(post.getUser().getId())) {
             return AuthorTag.AUTHOR;
         }
         return AuthorTag.NORMAL;
