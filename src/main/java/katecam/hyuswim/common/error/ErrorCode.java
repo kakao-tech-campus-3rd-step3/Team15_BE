bfabf62fe9c97809b6ebfc350f8c27c642b3ab81
kimpro4214 <qnfghk555@naver.com>
2025-09-16 15:40:58 +0900

    feat: 신고 에러 코드 추가

--- src/main/java/katecam/hyuswim/common/error/ErrorCode.java
+++ src/main/java/katecam/hyuswim/common/error/ErrorCode.java
@@ -29,6 +29,7 @@ public enum ErrorCode {
     //Report
     INVALID_REPORT_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않은 신고 타입입니다."),
     REPORT_REASON_REQUIRED(HttpStatus.BAD_REQUEST, "기타 사유 선택 시 내용은 필수입니다."),
+    REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 신고 내역입니다."),
 
     //Common
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다.");
