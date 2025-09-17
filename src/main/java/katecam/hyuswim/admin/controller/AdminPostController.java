e22f459c0675acb0990308b19ebb9a8097310adf
kimpro4214 <qnfghk555@naver.com>
2025-09-17 15:26:28 +0900

    feat: 관리자 페이지 게시판 조회 /상세조회 기능 추가

---
+++ src/main/java/katecam/hyuswim/admin/controller/AdminPostController.java
@@ -0,0 +1,33 @@
+package katecam.hyuswim.admin.controller;
+
+import katecam.hyuswim.admin.dto.AdminPostDetailResponse;
+import katecam.hyuswim.admin.dto.AdminPostListResponse;
+import katecam.hyuswim.admin.service.AdminPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/posts")
+public class AdminPostController {
+
+    private final AdminPostService adminPostService;
+
+    @GetMapping
+    public String posts(Model model) {
+        List<AdminPostListResponse> posts = adminPostService.getPosts();
+        model.addAttribute("posts", posts);
+        return "admin/posts/list";
+    }
+
+    @GetMapping("/{postId}")
+    public String post(@PathVariable Long postId, Model model) {
+        AdminPostDetailResponse post = adminPostService.getPost(postId);
+        model.addAttribute("post", post);
+        return "admin/posts/detail";
+    }
+}
