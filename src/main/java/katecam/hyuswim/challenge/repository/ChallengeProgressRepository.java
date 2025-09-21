8d63845cedfd8a3a00cbd78d82aae8c3628adb92
hwangyungyu <vlzkcbdmdkr@naver.com>
2025-09-21 20:09:06 +0900

    refactor : 피드백 반영

--- src/main/java/katecam/hyuswim/challenge/repository/ChallengeProgressRepository.java
+++ src/main/java/katecam/hyuswim/challenge/repository/ChallengeProgressRepository.java
@@ -2,11 +2,14 @@ package katecam.hyuswim.challenge.repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import katecam.hyuswim.challenge.domain.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import katecam.hyuswim.challenge.domain.ChallengeProgress;
+import katecam.hyuswim.user.domain.User;
 
 public interface ChallengeProgressRepository extends JpaRepository<ChallengeProgress, Long> {
-    Optional<ChallengeProgress> findByUserIdAndChallengeId(Long userId, Long challengeId);
-    List<ChallengeProgress> findByUserId(Long userId);
+    Optional<ChallengeProgress> findByUserAndChallenge(User user, Challenge challenge);
+    List<ChallengeProgress> findByUser(User user);
 }
 
