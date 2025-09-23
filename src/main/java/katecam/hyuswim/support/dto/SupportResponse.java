15013081af37a301d5a360021a6bf2e77ae35519
kimpro4214 <qnfghk555@naver.com>
2025-09-24 00:39:53 +0900

    feat: support response/detailresponse dto 추가

---
+++ src/main/java/katecam/hyuswim/support/dto/SupportResponse.java
@@ -0,0 +1,31 @@
+package katecam.hyuswim.support.dto;
+
+import katecam.hyuswim.support.domain.Support;
+import katecam.hyuswim.support.domain.SupportType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class SupportResponse {
+
+    private Long id;
+    private String name;
+    private String company;
+    private SupportType supportType;
+    private LocalDate endPoint;
+
+    public static SupportResponse from(Support support) {
+        return SupportResponse.builder()
+                .id(support.getId())
+                .name(support.getName())
+                .company(support.getCompany())
+                .supportType(support.getSupportType())
+                .endPoint(support.getEndPoint())
+                .build();
+    }
+}
