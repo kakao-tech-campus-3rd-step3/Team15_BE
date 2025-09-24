a66aa62ee9df2e822a6520e2b4b78eb8c0f90459
kimpro4214 <qnfghk555@naver.com>
2025-09-24 10:53:09 +0900

    refactor: 진행 중인 지원 사업 개수 조회

--- src/main/java/katecam/hyuswim/support/repository/SupportRepository.java
+++ src/main/java/katecam/hyuswim/support/repository/SupportRepository.java
@@ -3,10 +3,13 @@ package katecam.hyuswim.support.repository;
 import katecam.hyuswim.support.domain.Support;
 import katecam.hyuswim.support.domain.SupportType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SupportRepository extends JpaRepository<Support, Long> {
 
     List<Support> findBySupportType(SupportType supportType);
+    @Query("SELECT COUNT(s) FROM Support s WHERE s.endPoint >= CURRENT_DATE")
+    long countActiveSupports();
 }
