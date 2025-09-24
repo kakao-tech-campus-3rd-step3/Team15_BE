1167dc87e77c7dc67b837a1f21724df194cb3824
kimpro4214 <qnfghk555@naver.com>
2025-09-24 13:38:08 +0900

    refactor: EndPoint -> EndDate

--- src/main/java/katecam/hyuswim/support/repository/SupportRepository.java
+++ src/main/java/katecam/hyuswim/support/repository/SupportRepository.java
@@ -5,11 +5,14 @@ import katecam.hyuswim.support.domain.SupportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SupportRepository extends JpaRepository<Support, Long> {
 
     List<Support> findBySupportType(SupportType supportType);
-    @Query("SELECT COUNT(s) FROM Support s WHERE s.endPoint >= CURRENT_DATE")
+    @Query("SELECT COUNT(s) FROM Support s WHERE s.endDate >= CURRENT_DATE")
     long countActiveSupports();
+
+    long countByEndDateAfter(LocalDate date);
 }
