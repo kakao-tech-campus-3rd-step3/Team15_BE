4013147d4b20fd44b05c78ff12e4c2748d10e118
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-09-27 17:06:23 +0900

    chore: frontendurl 복수형으로 변경

--- src/main/java/katecam/hyuswim/common/config/AppProperties.java
+++ src/main/java/katecam/hyuswim/common/config/AppProperties.java
@@ -1,7 +1,6 @@
 package katecam.hyuswim.common.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
